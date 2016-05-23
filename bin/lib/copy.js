c62f838ed3470bbd5bf417ef9e9d1eea9b1ced5c
maxlath <k@maxlath.eu>
2016-05-23 18:01:31 +0200

    preventing to write "copied to Clipboard" to stdin

--- bin/lib/copy.js
+++ bin/lib/copy.js
@@ -1,8 +1,28 @@
 var copy = require('copy-paste').copy
-require('colors')
+var exec = require('child_process').exec
+
+// Force colors as playing with /dev/tty might disable it
+require('colors').enabled = true
+var label = 'copied to Clipboard: '.green
+var options = ''
 
 module.exports = function (text) {
   copy(text)
-  process.stdout.write('copied to Clipboard: '.green + text + '\n')
-  process.exit()
+
+  // If stdout isn't the terminal
+  // write a copy of the text to the terminal anyway
+  if (!(process.stdout.isTTY)) {
+    if (text.length > 20) {
+      label += text.substring(0,20) + '...'
+    } else {
+      label += text
+    }
+  } else {
+    options = '-n'
+  }
+
+  exec(`echo ${options} ${label} > /dev/tty`, function () {
+    console.log(text)
+    process.exit(0)
+  })
 }
