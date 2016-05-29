b9a59c6d017e17a5344278a75fcdc514f3dce3cb
maxlath <k@maxlath.eu>
2016-05-29 12:21:50 +0200

    extracting clipboard modules to their own npm module: copy2cb

--- bin/lib/copy.js
+++ bin/lib/copy.js
@@ -1,25 +1,7 @@
-var copy = require('copy-paste').copy
-var tty = require('./direct_tty')
-var label = 'copied to Clipboard: '.green
+var cb = require('copy2cb')
 
 module.exports = function (text) {
-  copy(text)
-
-  // If stdout isn't the terminal
-  // write an extract of the text to the terminal
-  if (!(process.stdout.isTTY)) {
-    if (text.length > 20) {
-      label += text.substring(0,20) + '...'
-    } else {
-      label += text
-    }
-    label += '\n'
-  }
-
-  tty.write(label, function () {
-    // Using console.log instead of process.stdout.write
-    // as it will block and only once done exiting the process
-    console.log(text)
+  cb(text, function() {
     process.exit(0)
   })
 }
