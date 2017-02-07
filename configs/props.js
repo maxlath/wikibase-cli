209b725004bed660b944c0c172664f9e5ed9d76e
maxlath <k@maxlath.eu>
2017-02-07 16:52:56 +0100

    wd props: add the 'type' option to include properties data types

--- configs/props.js
+++ configs/props.js
@@ -6,6 +6,7 @@ module.exports = {
   },
   examples: [
     { args: '', comment: 'output all the properties' },
+    { args: '--type', comment: 'output all the properties and their types' },
     { args: '| grep image', comment: 'filter the properties to keep only those related to images' },
     { args: '--lang sv', comment: 'output all the properties with their label in Swedish' },
     { args: '--reset', comment: 'delete all the cached properties files to get fresh versions on the next execution' }
--- test/wd-props.js
+++ test/wd-props.js
@@ -6,10 +6,19 @@ test('wd props --help: display help', t => {
   .then(res => t.deepEqual(res.stdout.split('Usage:').length, 2))
 })
 
-test('wd data', t => {
+test('wd props', t => {
   return execa.shell('./bin/wd props')
   .then(res => {
     t.true(res.stdout.startsWith('{'))
     t.true(res.stdout.split('"P6": "').length === 2)
+  })
+})
+
+test('wd props --type', t => {
+  return execa.shell('./bin/wd props --type -l en')
+  .then(res => {
+    const data = JSON.parse(res.stdout)
+    t.is(data.P6.label, 'head of government')
+    t.is(data.P6.type, 'WikibaseItem')
   })
 })
