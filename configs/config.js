7c4a946db8bd557bb02baf7a8b84c9140a1276b2
maxlath <k@maxlath.eu>
2017-02-17 23:02:24 +0100

    introducing 'wd-config' to persist options: starting with clipboard

---
+++ configs/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  args: '<key> [value]',
+  description: 'get and set configuration parameters',
+  options: {
+    lang: false
+  },
+  examples: [
+    { args: 'clipboard true', comment: 'always activate the clipboard when possible' }
+  ]
+}
