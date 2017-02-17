f9341b79e585244600977ea3269b36e0ddb31b32
maxlath <k@maxlath.eu>
2017-02-17 18:40:51 +0100

    unknown command: log error and show help

--- test/wd.js
+++ test/wd.js
@@ -5,3 +5,11 @@ test('wd: display help', t => {
   return execa.shell('./bin/wd')
   .then(res => t.deepEqual(res.stdout.split('Usage:').length, 2))
 })
+
+test('wd unknown command: log error and display help', t => {
+  return execa.shell('./bin/wd nop')
+  .then(res => {
+    t.is(res.stderr, 'unknown command')
+    t.deepEqual(res.stdout.split('Usage:').length, 2)
+  })
+})
