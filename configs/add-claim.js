5701da2313ed7a45d71078f5b159345a0d47a1c0
maxlath <k@maxlath.eu>
2017-02-19 22:15:18 +0100

    added first write operation: wd add-claim wooooooooooooooooooohoooooo \o/

---
+++ configs/add-claim.js
@@ -0,0 +1,10 @@
+module.exports = {
+  args: '<entity> <property> <value>',
+  description: 'add a claim to an entity',
+  options: {
+    lang: false
+  },
+  examples: [
+    { args: 'Q4115189 P2002 Zorglub', comment: "add the Twitter account (P2002) 'Zorglub' to the Sandbox (Q4115189) entity" }
+  ]
+}
