b8235929c1d6dab771722d6dd72a1a838a998b66
maxlath <k@maxlath.eu>
2017-02-20 12:16:00 +0100

    wd-query test: lower the chance of failing test

--- test/wd-query.js
+++ test/wd-query.js
@@ -7,7 +7,7 @@ test('wd query: display help', t => {
 })
 
 test('wd query -p [prop] -o [obj] -t [limit]', t => {
-  return execa.shell('./bin/wd query -p P31 -o Q44559 --limit 5')
+  return execa.shell('./bin/wd query -p P31 -o Q44559 --limit 100')
   .then(res => {
     t.is(res.stdout.split('Q47304').length, 2)
   })
