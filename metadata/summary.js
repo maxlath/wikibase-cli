f22537452fe1bb4e541ae4aa89c665cd7a401701
maxlath <k@maxlath.eu>
2017-10-13 18:21:51 +0200

    wd-summary: allow to request additional properties

--- metadata/summary.js
+++ metadata/summary.js
@@ -6,6 +6,7 @@ module.exports = {
   },
   examples: [
     { args: 'Q123', comment: 'display basic information for the entity Q123' },
-    { args: 'Q123 --lang ja', comment: 'display basic information for the entity Q123 in Japanese' }
+    { args: 'Q123 --lang ja', comment: 'display basic information for the entity Q123 in Japanese' },
+    { args: 'Q1 -p P3219,P2612', comment: 'display the summary with additional properties' }
   ]
 }
