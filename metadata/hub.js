04bb3230c4fc324ced25393ea6d6ce3c15888d77
maxlath <k@maxlath.eu>
2018-09-24 17:06:07 +0200

    wd hub: default to outputting the destination url
    addressing #54 https://github.com/maxlath/hub/issues/7

--- metadata/hub.js
+++ metadata/hub.js
@@ -4,13 +4,14 @@ module.exports = {
   options: {
     lang: true,
     verbose: false,
-    clipboard: false,
+    clipboard: true,
     json: true,
     instance: false,
     sparqlEndpoint: false
   },
   examples: [
-    { args: 'viaf:24597135 s=inv', comment: 'Find the entity having 24597135 as VIAF id and open the corresponding page on inventaire.io' },
-    { args: 'Q3 property=image width=300 --json | jd destination.url', comment: 'Get the image illustrating Q3 in 300px' }
+    { args: 'viaf:24597135', comment: 'Find the entity having 24597135 as VIAF id' },
+    { args: '--open viaf:24597135 site=inventaire', comment: 'Find the entity having 24597135 as VIAF id and open the corresponding page on inventaire.io' },
+    { args: 'Q3 property=image width=300', comment: 'Get the image illustrating Q3 in 300px' }
   ]
 }
