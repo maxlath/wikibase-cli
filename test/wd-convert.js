31589474232c3d46c7e52c2d1dd81dd6e2e7fe21
maxlath <k@maxlath.eu>
2018-10-05 14:37:21 +0200

    wd convert: support converting from Wikidata ids
    changing the map value to an array to account for keys that have multiple values

--- test/wd-convert.js
+++ test/wd-convert.js
@@ -9,8 +9,8 @@ describe('wd convert', function () {
     .then(res => {
       const results = JSON.parse(res.stdout)
       results.should.be.an.Object()
-      results['11865344k'].should.equal('Q45')
-      results['11932251d'].should.equal('Q140')
+      results['11865344k'][0].should.equal('Q45')
+      results['11932251d'][0].should.equal('Q140')
       done()
     })
     .catch(done)
@@ -21,8 +21,21 @@ describe('wd convert', function () {
     .then(res => {
       const results = JSON.parse(res.stdout)
       results.should.be.an.Object()
-      results['11865344k'].should.equal('Q45')
-      results['11932251d'].should.equal('Q140')
+      results['11865344k'][0].should.equal('Q45')
+      results['11932251d'][0].should.equal('Q140')
+      done()
+    })
+    .catch(done)
+  })
+
+  it('should convert Wikidata ids', done => {
+    execa.shell('./bin/wd convert P268 Q45 Q140')
+    .then(res => {
+      const results = JSON.parse(res.stdout)
+      results.should.be.an.Object()
+      results.Q45.includes('11865344k').should.be.true()
+      results.Q45.includes('119328454').should.be.true()
+      results.Q140[0].should.equal('11932251d')
       done()
     })
     .catch(done)
