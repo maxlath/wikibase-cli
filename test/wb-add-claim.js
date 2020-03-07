ede699dda5f148ca47d2b7b7e625011e081276ae
maxlath <k@maxlath.eu>
2020-03-07 21:49:01 +0100

    tests: write ops: run tests on test.wikidata.org

--- test/wb-add-claim.js
+++ test/wb-add-claim.js
@@ -1,24 +1,24 @@
 require('should')
-const { shellExec } = require('./lib/utils')
+const { wdTest } = require('./lib/utils')
 
 describe('wb edit-entity', function () {
   this.timeout(20000)
 
   it('should add a claim', async () => {
-    const { stdout, stderr } = await shellExec('./bin/wd add-claim Q4115189 P370 "add-claim test"')
+    const { stdout, stderr } = await wdTest('add-claim Q1111 P95180 "add-claim test"')
     stderr.should.equal('')
     const res = JSON.parse(stdout)
-    res.claim.id.should.startWith('Q4115189')
+    res.claim.id.should.startWith('Q1111')
   })
 
   it('should take arguments from stdin in batch mode', async () => {
-    const { stdout, stderr } = await shellExec('./bin/wd add-claim --batch < ./test/assets/add_claim_batch')
+    const { stdout, stderr } = await wdTest('add-claim --batch < ./test/assets/add_claim_batch')
     const [ res1, res2 ] = stdout.split('\n')
-    JSON.parse(res1).claim.id.should.startWith('Q4115189')
-    JSON.parse(res2).claim.id.should.startWith('Q4115189')
+    JSON.parse(res1).claim.id.should.startWith('Q1111')
+    JSON.parse(res2).claim.id.should.startWith('Q1111')
     const [ log1, log2, logDone ] = stderr.split('\n')
-    log1.trim().should.equal('processing line 1: Q4115189 P370 bar')
-    log2.trim().should.equal('processing line 2: Q4115189 P1106 789')
+    log1.trim().should.equal('processing line 1: Q1111 P95180 bar')
+    log2.trim().should.equal('processing line 2: Q1111 P95228 789')
     logDone.trim().should.equal('done processing 2 lines')
   })
 })
