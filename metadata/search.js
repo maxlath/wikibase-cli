652995b719bc4fd52412d7448d3df538820a4f3e
maxlath <k@maxlath.eu>
2020-04-17 23:57:19 +0200

    wb-search: refactor type parser

--- metadata/search.js
+++ metadata/search.js
@@ -14,6 +14,10 @@ module.exports = {
     { args: 'Ligo', comment: 'displays a list of entities matching "Ligo"' },
     { args: 'Harry Potter --limit 25', comment: 'displays up to 25 results matching "Harry Potter"' },
     { args: 'Harry Potter --verbose', comment: 'display rich results (aka summaries)' },
-    { args: 'Harry Potter --properties P577,P935', comment: 'request additional properties (separated by a comma) to be added to the results summaries' }
+    { args: 'Harry Potter --properties P577,P935', comment: 'request additional properties (separated by a comma) to be added to the results summaries' },
+    { args: 'date --type property', comment: 'Search properties (but `wb props` might be doing a better job)' },
+    { args: 'date --type lexeme', comment: 'Search lexemes' },
+    { args: 'code --type form', comment: 'Search forms' },
+    { args: 'test --type sense', comment: "Searching senses doesn't seem to work currently (2020-04-17)" }
   ]
 }
