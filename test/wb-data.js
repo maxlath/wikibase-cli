7799c84914aaa2308744735074caab572902d70f
maxlath <k@maxlath.eu>
2020-04-18 12:55:34 +0200

    tests: refactor with async/await

--- test/wb-data.js
+++ test/wb-data.js
@@ -11,172 +11,120 @@ describe('wb data', function () {
   // and the script thus will listen for stdin. But it works when run manually,
   // and no other way to detect a stdin input was found
 
-  // it('should display help', done => {
-  //   shellExec('./bin/wd data')
-  //   .then(res => {
-  //     res.stdout.split('Usage:').length.should.equal(2)
-  //     done()
-  //   })
-  //   .catch(done)
+  // it('should display help', async () => {
+  //   const { stdout } = await shellExec('./bin/wd data')
+  //   stdout.split('Usage:').length.should.equal(2)
   // })
 
-  it('<entity>', done => {
-    shellExec('./bin/wd data Q123456')
-    .then(res => {
-      res.stdout.startsWith('{').should.be.true()
-      const data = JSON.parse(res.stdout)
-      const dataAttrs = Object.keys(data)
-      attributes.length.should.equal(dataAttrs.length)
-      dataAttrs.forEach(attr => should(attributes.indexOf(attr) > -1).be.true())
-      done()
-    })
-    .catch(done)
+  it('<entity>', async () => {
+    const { stdout } = await shellExec('./bin/wd data Q123456')
+    stdout.startsWith('{').should.be.true()
+    const data = JSON.parse(stdout)
+    const dataAttrs = Object.keys(data)
+    attributes.length.should.equal(dataAttrs.length)
+    dataAttrs.forEach(attr => should(attributes.indexOf(attr) > -1).be.true())
   })
 
-  it('should accept several ids', done => {
-    shellExec('./bin/wd data Q123456 Q123')
-    .then(res => {
-      const lines = res.stdout.split('\n')
-      lines.length.should.equal(2)
-      lines.forEach(line => {
-        Object.keys(JSON.parse(line)).forEach(attr => {
-          should(attributes.includes(attr)).be.true()
-        })
+  it('should accept several ids', async () => {
+    const { stdout } = await shellExec('./bin/wd data Q123456 Q123')
+    const lines = stdout.split('\n')
+    lines.length.should.equal(2)
+    lines.forEach(line => {
+      Object.keys(JSON.parse(line)).forEach(attr => {
+        should(attributes.includes(attr)).be.true()
       })
-      done()
     })
-    .catch(done)
   })
 
-  it('should output entities as ndjson', done => {
-    shellExec('./bin/wd data Q123456 Q1512522')
-    .then(res => {
-      res.stdout.split('\n').length.should.equal(2)
-      done()
-    })
-    .catch(done)
+  it('should output entities as ndjson', async () => {
+    const { stdout } = await shellExec('./bin/wd data Q123456 Q1512522')
+    stdout.split('\n').length.should.equal(2)
   })
 
-  it('should simplify entity when requested', done => {
-    shellExec('./bin/wd data Q1512522 --simplify')
-    .then(res => {
-      const entity = JSON.parse(res.stdout)
-      entity.labels.de.should.be.a.String()
-      entity.descriptions.de.should.be.a.String()
-      entity.aliases.de[0].should.be.a.String()
-      entity.claims.P31[0].should.be.a.String()
-      done()
-    })
-    .catch(done)
+  it('should simplify entity when requested', async () => {
+    const { stdout } = await shellExec('./bin/wd data Q1512522 --simplify')
+    const entity = JSON.parse(stdout)
+    entity.labels.de.should.be.a.String()
+    entity.descriptions.de.should.be.a.String()
+    entity.aliases.de[0].should.be.a.String()
+    entity.claims.P31[0].should.be.a.String()
   })
 
-  it('should simplify entities when requested', done => {
-    shellExec('./bin/wd data Q1512522 Q123456 --simplify')
-    .then(res => {
-      const entity = JSON.parse(res.stdout.split('\n')[0])
-      entity.labels.de.should.be.a.String()
-      entity.descriptions.de.should.be.a.String()
-      entity.aliases.de[0].should.be.a.String()
-      entity.claims.P31[0].should.be.a.String()
-      done()
-    })
-    .catch(done)
+  it('should simplify entities when requested', async () => {
+    const { stdout } = await shellExec('./bin/wd data Q1512522 Q123456 --simplify')
+    const entity = JSON.parse(stdout.split('\n')[0])
+    entity.labels.de.should.be.a.String()
+    entity.descriptions.de.should.be.a.String()
+    entity.aliases.de[0].should.be.a.String()
+    entity.claims.P31[0].should.be.a.String()
   })
 
-  it('should return only the desired props when requested', done => {
-    shellExec('./bin/wd data Q1512522 --props labels,aliases')
-    .then(res => {
-      const entity = JSON.parse(res.stdout)
+  it('should return only the desired props when requested', async () => {
+    const { stdout } = await shellExec('./bin/wd data Q1512522 --props labels,aliases')
+    const entity = JSON.parse(stdout)
+    should(entity.labels).be.an.Object()
+    should(entity.descriptions).not.be.ok()
+    should(entity.aliases).be.an.Object()
+    should(entity.descriptions).not.be.ok()
+    should(entity.sitelinks).not.be.ok()
+  })
+
+  it('should accept ids on stdin', async () => {
+    const { stdout } = await shellExec('echo "Q123456 Q123" | ./bin/wd data --props labels --simplify')
+    const lines = stdout.split('\n')
+    lines.length.should.equal(2)
+    const entities = lines.map(JSON.parse.bind(JSON))
+    entities[0].id.should.equal('Q123456')
+    entities[1].id.should.equal('Q123')
+    entities.forEach(entity => {
       should(entity.labels).be.an.Object()
+      _.values(entity.labels).forEach(label => {
+        label.should.be.a.String()
+      })
       should(entity.descriptions).not.be.ok()
-      should(entity.aliases).be.an.Object()
+      should(entity.aliases).not.be.ok()
       should(entity.descriptions).not.be.ok()
       should(entity.sitelinks).not.be.ok()
-      done()
     })
-    .catch(done)
   })
 
-  it('should accept ids on stdin', done => {
-    shellExec('echo "Q123456 Q123" | ./bin/wd data --props labels --simplify')
-    .then(res => {
-      const lines = res.stdout.split('\n')
-      lines.length.should.equal(2)
-      const entities = lines.map(JSON.parse.bind(JSON))
-      entities[0].id.should.equal('Q123456')
-      entities[1].id.should.equal('Q123')
-      entities.forEach(entity => {
-        should(entity.labels).be.an.Object()
-        _.values(entity.labels).forEach(label => {
-          label.should.be.a.String()
-        })
-        should(entity.descriptions).not.be.ok()
-        should(entity.aliases).not.be.ok()
-        should(entity.descriptions).not.be.ok()
-        should(entity.sitelinks).not.be.ok()
-      })
-      done()
-    })
-    .catch(done)
-  })
-
-  it('should return ttl when requested', done => {
-    shellExec('./bin/wd data Q1512522 --format ttl')
-    .then(res => {
-      res.stdout.should.startWith('@prefix rdf:')
-      done()
-    })
-    .catch(done)
+  it('should return ttl when requested', async () => {
+    const { stdout } = await shellExec('./bin/wd data Q1512522 --format ttl')
+    stdout.should.startWith('@prefix rdf:')
   })
 
   describe('claim data', () => {
-    it('should get a claim data provided a claim guid', done => {
+    it('should get a claim data provided a claim guid', async () => {
       const guid = 'Q2$50fad68d-4f91-f878-6f29-e655af54690e'
-      shellExec(`./bin/wd data '${guid}'`)
-      .then(res => {
-        const claim = JSON.parse(res.stdout)
-        claim.id.should.equal(guid)
-        claim.mainsnak.property.should.equal('P31')
-        claim.mainsnak.datavalue.value.id.should.equal('Q3504248')
-        done()
-      })
-      .catch(done)
+      const { stdout } = await shellExec(`./bin/wd data '${guid}'`)
+      const claim = JSON.parse(stdout)
+      claim.id.should.equal(guid)
+      claim.mainsnak.property.should.equal('P31')
+      claim.mainsnak.datavalue.value.id.should.equal('Q3504248')
     })
 
-    it('should accept prefixed claim GUIDs', done => {
+    it('should accept prefixed claim GUIDs', async () => {
       const prefixedGuid = 'wds:Q2-50fad68d-4f91-f878-6f29-e655af54690e'
-      shellExec(`./bin/wd data '${prefixedGuid}'`)
-      .then(res => {
-        const claim = JSON.parse(res.stdout)
-        claim.id.should.equal('Q2$50fad68d-4f91-f878-6f29-e655af54690e')
-        claim.mainsnak.property.should.equal('P31')
-        claim.mainsnak.datavalue.value.id.should.equal('Q3504248')
-        done()
-      })
-      .catch(done)
+      const { stdout } = await shellExec(`./bin/wd data '${prefixedGuid}'`)
+      const claim = JSON.parse(stdout)
+      claim.id.should.equal('Q2$50fad68d-4f91-f878-6f29-e655af54690e')
+      claim.mainsnak.property.should.equal('P31')
+      claim.mainsnak.datavalue.value.id.should.equal('Q3504248')
     })
 
-    it('should get a simplified claim', done => {
-      shellExec(`./bin/wd data --simplify 'Q2$50fad68d-4f91-f878-6f29-e655af54690e'`)
-      .then(res => {
-        res.stdout.should.equal('Q3504248')
-        done()
-      })
-      .catch(done)
+    it('should get a simplified claim', async () => {
+      const { stdout } = await shellExec(`./bin/wd data --simplify 'Q2$50fad68d-4f91-f878-6f29-e655af54690e'`)
+      stdout.should.equal('Q3504248')
     })
 
-    it('should keep the requested simplified claim data', done => {
+    it('should keep the requested simplified claim data', async () => {
       const guid = 'Q2$50fad68d-4f91-f878-6f29-e655af54690e'
-      shellExec(`./bin/wd data --simplify --keep ids,references,qualifiers '${guid}'`)
-      .then(res => {
-        const claim = JSON.parse(res.stdout)
-        claim.id.should.equal(guid)
-        claim.value.should.equal('Q3504248')
-        claim.references.should.be.an.Array()
-        claim.qualifiers.should.be.an.Object()
-        done()
-      })
-      .catch(done)
+      const { stdout } = await shellExec(`./bin/wd data --simplify --keep ids,references,qualifiers '${guid}'`)
+      const claim = JSON.parse(stdout)
+      claim.id.should.equal(guid)
+      claim.value.should.equal('Q3504248')
+      claim.references.should.be.an.Array()
+      claim.qualifiers.should.be.an.Object()
     })
   })
 })
