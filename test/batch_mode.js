90dea6882a1e7435c4e204e19f9b1bc321b0a2f7
maxlath <k@maxlath.eu>
2020-07-11 10:17:46 +0200

    tests: batch mode: cleanup

--- test/batch_mode.js
+++ test/batch_mode.js
@@ -3,26 +3,30 @@ const { wdTest } = require('./lib/utils')
 
 describe('batch mode', function () {
   this.timeout(20000)
+
   it('should take arguments from stdin (add-claim)', async () => {
     const { stdout, stderr } = await wdTest('add-claim --batch < ./test/assets/add_claim_batch')
     const [ res1, res2 ] = stdout.split('\n')
     JSON.parse(res1).claim.id.should.startWith('Q1111')
     JSON.parse(res2).claim.id.should.startWith('Q1111')
-    const [ log1, log2, logDone ] = stderr.split('\n')
-    log1.trim().should.equal('processing line 1: Q1111 P95180 bar')
-    log2.trim().should.equal('processing line 2: [ "Q1111", "P95226", {"time":"1800","precision":7} ]')
-    logDone.trim().should.equal('done processing 2 lines: successes=2 errors=0')
+    stderr.split('\n').should.deepEqual([
+      'processing line 1: Q1111 P95180 bar',
+      'processing line 2: [ "Q1111", "P95226", {"time":"1800","precision":7} ]',
+      'done processing 2 lines: successes=2 errors=0'
+    ])
   })
 
   it('should take arguments from stdin (edit-entity)', async () => {
-    const { stdout, stderr } = await wdTest('edit-entity --batch < ./test/assets/edit_entity_batch')
-    const [ res1, res2 ] = stdout.split('\n')
-    JSON.parse(res1).entity.id.should.equal('Q1111')
-    JSON.parse(res2).entity.id.should.equal('Q1111')
-    const [ log1, log2, logDone ] = stderr.split('\n')
-    log1.trim().should.equal('processing line 1: {"id":"Q1111", "claims":{ "P95180": "foo" }}')
-    log2.trim().should.equal('processing line 2: {"id":"Q1111", "claims":{ "P95228": 456 }}')
-    logDone.trim().should.equal('done processing 2 lines: successes=2 errors=0')
+    const { stdout, stderr } = await wdTest('edit-entity --batch --dry < ./test/assets/edit_entity_batch')
+    stdout.split('\n').should.deepEqual([
+      '{"id":"Q1111","claims":{"P95180":"foo"}}',
+      '{"id":"Q1111","claims":{"P95228":456}}'
+    ])
+    stderr.split('\n').should.deepEqual([
+      'processing line 1: {"id":"Q1111", "claims":{ "P95180": "foo" }}',
+      'processing line 2: {"id":"Q1111", "claims":{ "P95228": 456 }}',
+      'done processing 2 lines: successes=2 errors=0'
+    ])
   })
 
   it('should take some arguments inline and complete with stdin', async () => {
