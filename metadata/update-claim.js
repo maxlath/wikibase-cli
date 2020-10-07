e3d0f0af6de8317e9539670e322b1b9cbc52288b
maxlath <k@maxlath.eu>
2020-10-07 16:20:52 +0200

    metadata: update-claim: default to '<guid> <new-value>'

--- metadata/update-claim.js
+++ metadata/update-claim.js
@@ -2,10 +2,11 @@ const { editCommands } = require('../lib/common_options')
 
 module.exports = {
   alias: 'uc',
-  args: '<entity> <property> <old-value> <new-value>',
+  args: '<guid> <new-value>',
   description: "update a claim's value",
   options: Object.assign({ rank: true }, editCommands),
   examples: [
+    { args: 'Q4115189$bc43ae33-7525-42cb-ad31-b1e1cf3cb72d Bulgroz', comment: "update the claim identified by this id with the value 'Bulgroz'" },
     { args: 'Q4115189 P2002 Zorglub Bulgroz', comment: "change the the Sandbox (Q4115189) Twitter account (P2002) from 'Zorglub' to 'Bulgroz'" }
   ]
 }
