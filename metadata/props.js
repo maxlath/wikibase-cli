7afe6b95f0328a9a7770ab2e15fe2b6ec410f4fb
maxlath <k@maxlath.eu>
2022-01-16 17:18:07 +0100

    wb-props: allow to filter by both type and pattern

--- metadata/props.js
+++ metadata/props.js
@@ -16,6 +16,7 @@ module.exports = {
     { args: '--details', comment: 'output all properties, with their labels, types, descriptions, and aliases' },
     { args: 'image', comment: 'filter the properties to keep only those with labels matching "image"' },
     { args: '--type Url', comment: 'filter the properties to keep only those of type Url (other possible types: ExternalId, String, WikibaseItem, WikibaseProperty, Time, Monolingualtext, Quantity, CommonsMedia, GlobeCoordinate)' },
+    { args: '--type Url ref', comment: 'filter the properties to keep only those of type Url matching the pattern "ref"' },
     { args: '--lang sv', comment: 'output all the properties with their label in Swedish' },
     { args: '--no-cache', comment: 'ignore properties cache: fetch properties from the SPARQL endpoint, even if already cached' },
     { args: '--reset', comment: 'delete all the cached properties files to get fresh versions on the next execution' }
