e351da06701cd80d353944c56d13d87fa9e79753
maxlath <k@maxlath.eu>
2022-01-16 17:21:29 +0100

    wb-claims: allow to filter claims by both type and pattern

--- metadata/claims.js
+++ metadata/claims.js
@@ -13,7 +13,7 @@ module.exports = {
   examples: [
     { args: 'Q2001', comment: 'fetch all claims for the entity Q2001' },
     { args: 'Q2001 P106', comment: 'or just the claims for the property P106' },
-    { args: 'Q2001 Url', comment: 'or just the claims from properties of type Url (other possible types: ExternalId, String, WikibaseItem, WikibaseProperty, Time, Monolingualtext, Quantity, CommonsMedia, GlobeCoordinate)' },
+    { args: 'Q2001 --type Url', comment: 'or just the claims from properties of type Url (other possible types: ExternalId, String, WikibaseItem, WikibaseProperty, Time, Monolingualtext, Quantity, CommonsMedia, GlobeCoordinate)' },
     { args: 'Q2001 library', comment: 'or just the claims from properties with labels matching "library"' },
     { args: 'Q2001 website', comment: 'or website, etc' },
     { args: 'Q2001 --lang de', comment: 'fetch all claims for the entity Q2001 with properties labels in German' }
