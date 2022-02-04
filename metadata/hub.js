2a0383a6b2dd6366d8cc5a0a93340d175abe1261
maxlath <k@maxlath.eu>
2022-02-04 19:57:11 +0100

    wb hub: add --url option

--- metadata/hub.js
+++ metadata/hub.js
@@ -13,6 +13,7 @@ module.exports = {
   examples: [
     { args: 'viaf:24597135', comment: 'Find the entity having 24597135 as VIAF id' },
     { args: '--open viaf:24597135 site=inventaire', comment: 'Find the entity having 24597135 as VIAF id and open the corresponding page on inventaire.io' },
-    { args: 'Q3 property=image width=300', comment: 'Get the image illustrating Q3 in 300px' }
+    { args: 'Q3 property=image width=300', comment: 'Get the image illustrating Q3 in 300px' },
+    { args: 'Q3 --url', comment: 'Just return the query URL' },
   ]
 }
--- test/wb-hub.js
+++ test/wb-hub.js
@@ -6,4 +6,9 @@ describe('wb hub', () => {
     const { stdout } = await shellExec('./bin/wb hub')
     stdout.should.containEql('Usage:')
   })
+
+  it('should return a url', async () => {
+    const { stdout } = await shellExec('./bin/wb hub --url 10.1103/REVMODPHYS.29.547 lang=de')
+    stdout.should.equal('https://tools.wmflabs.org/hub/query?q=10.1103%2FREVMODPHYS.29.547%20lang%3Dde%20site%3Dwikidata')
+  })
 })
