1bf302cd9ceb5ed063e15cc70ae64f3bfb516b1d
maxlath <k@maxlath.eu>
2022-03-16 08:07:08 +0100

    wb label metadata: add example with several entities

--- metadata/label.js
+++ metadata/label.js
@@ -5,6 +5,7 @@ module.exports = {
   options: require('../lib/common_options').entityAttributeCommands,
   examples: [
     { args: 'Q123', comment: 'fetch the label for the entity Q123' },
-    { args: 'Q123 --lang ja', comment: 'fetch the label for the entity Q123 in Japanese' }
+    { args: 'Q123 --lang ja', comment: 'fetch the label for the entity Q123 in Japanese' },
+    { args: 'Q1 Q2 Q3 P6', comment: 'fetch labels for several entities' },
   ]
 }
