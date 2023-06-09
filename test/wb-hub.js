192c01952addf47988e19ea56c3bd4ec81836852
maxlath <k@maxlath.eu>
2023-06-09 09:55:40 +0200

    tests: update wb-hub test

--- test/wb-hub.js
+++ test/wb-hub.js
@@ -9,6 +9,6 @@ describe('wb hub', () => {
 
   it('should return a url', async () => {
     const { stdout } = await shellExec('./bin/wb hub --url 10.1103/REVMODPHYS.29.547 lang=de')
-    stdout.should.equal('https://tools.wmflabs.org/hub/query?q=10.1103%2FREVMODPHYS.29.547%20lang%3Dde%20site%3Dwikidata')
+    stdout.should.equal('https://hub.toolforge.org/query?q=10.1103%2FREVMODPHYS.29.547%20lang%3Dde%20site%3Dwikidata')
   })
 })
