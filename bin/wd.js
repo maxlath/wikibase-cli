fa07c6b15c36db42b6ecb6e7413ddef15b4cf223
maxlath <k@maxlath.eu>
2023-07-18 10:40:29 +0200

    wd bin: fix executable name correction
    for the case where the executable doesn't have the .js extension
    
    Addressing #178

--- bin/wd.js
+++ bin/wd.js
@@ -3,6 +3,10 @@ process.env.WB_INSTANCE = 'https://www.wikidata.org'
 process.env.WB_SPARQL_ENDPOINT = 'https://query.wikidata.org/sparql'
 // Fake the executed script to be wb to trick commander to believe it
 // as it would otherwise fail to build subcommand paths
-process.argv[1] = process.argv[1].replace(/wd\.js$/, 'wb.js')
+process.argv[1] = process.argv[1]
+  // Case when called directly as ./bin/wd.js
+  .replace(/wd\.js$/, 'wb.js')
+  // Case when called directly as a link such as defined in package.json, ./node_modules/.bin/wd
+  .replace(/wd$/, 'wb')
 
 await import('./wb.js')
