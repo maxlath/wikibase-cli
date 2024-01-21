ea596f7c4f1a33b574119b1d6a2cfe01b52c7eb1
maxlath <k@maxlath.eu>
2024-01-21 18:09:11 +0100

    wb: refactor imports

--- bin/wb.js
+++ bin/wb.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-import fs from 'node:fs'
-import path, { resolve } from 'node:path'
+import { readdirSync } from 'node:fs'
+import { resolve } from 'node:path'
 import program from 'commander'
 import { debug } from '#lib/debug'
 import { getDirname } from '#lib/fs'
@@ -17,7 +17,7 @@ debug('program:metadata', { name: pkg.name, version: pkg.version })
 // To add a new command, create a file in ../metadata/${new-command-name}
 // and add an executable at ./wb-${new-command-name}
 
-const commandsNames = fs.readdirSync(path.resolve(dirname, '../metadata'))
+const commandsNames = readdirSync(resolve(dirname, '../metadata'))
   .map(filename => filename.replace('.js', ''))
 
 await Promise.all(commandsNames.map(async commandName => {
