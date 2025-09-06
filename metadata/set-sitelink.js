b8d7230011f0a4aab334646eb7307b863bbcb4d5
maxlath <k@maxlath.eu>
2025-09-06 11:13:17 +0200

    wb set-sitelink: help: add tip to get available badges list

--- metadata/set-sitelink.js
+++ metadata/set-sitelink.js
@@ -7,6 +7,6 @@ export default {
   options: editCommandsOptions,
   examples: [
     { args: 'Q4115189 enwikiquote "The Sandbox"', comment: "Link Q4115189 to the article 'The Sandbox' on the English Wikiquote" },
-    { args: 'Q4115189 enwikiquote "The Sandbox" Q17437796,Q17437798', comment: 'Also set badges for that sitelink' },
+    { args: 'Q4115189 enwikiquote "The Sandbox" Q17437796,Q17437798', comment: 'Also set badges for that sitelink (run command `wb badges` for a list of available badges' },
   ],
 }
